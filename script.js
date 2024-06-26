e14465fe5e297eb6abb3da1614ad1a16b3630352
miguelfsilvan <94316962+miguelfsilvan@users.noreply.github.com>
2024-06-26 14:12:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
